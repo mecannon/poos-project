1868830c7dee0c89998eef643b0b834071f806ba
Jonathan Padron <jo430544@ucf.edu>
2025-02-02 15:02:58 -0500

    Add files via upload

---
+++ API/DeleteContact.php
@@ -0,0 +1,36 @@
+<?php
+    $inData = getRequestInfo();
+
+    $ID = $inData["ID"];
+
+    $conn = new mysqli("localhost", "appuser", "SecurePass123", "contact");	
+    if($conn->connect_error){
+        returnWithError("error: Could not connect to database");
+    }
+    else{
+        $stmt = $conn->prepare("DELETE FROM contacts WHERE ID = ?");
+        $stmt->bind_param("i", $ID);
+        $stmt->execute();
+        sendResultInfoAsJson('{"result":"Finished Successfully"}');
+
+        $stmt->close();
+        $conn->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = '{"result":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+?>
---
+++ API/UpdateContact.php
@@ -0,0 +1,45 @@
+<?php
+    $inData = getRequestInfo();
+
+    $firstName = $inData["firstName"] ?? "";
+    $lastName = $inData["lastName"] ?? "";
+    $phone = $inData["phone"] ?? "";
+    $email = $inData["email"] ?? "";
+    $ID = $inData["ID"] ?? 0;
+
+    $conn = new mysqli("localhost", "appuser", "SecurePass123", "contact");		
+    if ($conn->connect_error) {
+        returnWithError("error: Could not connect to database");
+        exit();
+    }
+
+    // Update Contact
+    $stmt = $conn->prepare("UPDATE contacts SET FirstName = ?, LastName = ?, Phone = ?, Email = ? WHERE ID = ?");
+    $stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $ID);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        sendResultInfoAsJson(["result" => "Finished Successfully"]);
+    } else {
+        sendResultInfoAsJson(["result" => "No records updated. ID may not exist."]);
+    }
+
+    $stmt->close();
+    $conn->close();
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo json_encode($obj);
+    }
+
+    function returnWithError($err)
+    {
+        sendResultInfoAsJson(["result" => $err]);
+    }
+?>
